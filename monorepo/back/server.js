ac55a3cf8991547efd573e24e70e2eec8ca51777
TropikoW <jovannypersonal@gmail.com>
2024-08-06 23:22:15 -0500

    chore: Update server.js with HTTP request handling

--- monorepo/back/server.js
+++ monorepo/back/server.js
@@ -1,4 +1,10 @@
-const http = require('http')
+const http = require("http");
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

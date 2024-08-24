d42b58c9f8c3e8aa23c829f027bb3767567bf388
TropikoW <jovannypersonal@gmail.com>
2024-08-24 10:08:59 -0500

    chore: Update wp-config.php to use environment variables for database configuration

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -1,21 +1,42 @@
 <?php
-// ** Configuración de la base de datos ** //
-define('DB_NAME', getenv('DB_NAME'));
-define('DB_USER', getenv('DB_USER'));
-define('DB_PASSWORD', getenv('DB_PASSWORD'));
-define('DB_HOST', getenv('DB_HOST'));
-define('DB_CHARSET', 'utf8');
-define('DB_COLLATE', '');
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', getenv('DB_NAME') );
 
-// Configuración para PostgreSQL
-define('DB_TYPE', 'pgsql');
-define('DB_PORT', getenv('DB_PORT')); // Puerto para PostgreSQL
+/** MySQL database username */
+define( 'DB_USER', getenv('DB_USER') );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', getenv('DB_PASSWORD') );
+
+/** MySQL hostname */
+define( 'DB_HOST', getenv('DB_HOST') );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+define( 'AUTH_KEY',         getenv('AUTH_KEY') );
+define( 'SECURE_AUTH_KEY',  getenv('SECURE_AUTH_KEY') );
+define( 'LOGGED_IN_KEY',    getenv('LOGGED_IN_KEY') );
+define( 'NONCE_KEY',        getenv('NONCE_KEY') );
+define( 'AUTH_SALT',        getenv('AUTH_SALT') );
+define( 'SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT') );
+define( 'LOGGED_IN_SALT',   getenv('LOGGED_IN_SALT') );
+define( 'NONCE_SALT',       getenv('NONCE_SALT') );
 
 $table_prefix = 'wp_';
 
-define('WP_DEBUG', false);
+define( 'WP_DEBUG', getenv('WP_DEBUG') );
 
-if ( !defined('ABSPATH') )
-    define('ABSPATH', dirname(__FILE__) . '/');
+/* That's all, stop editing! Happy publishing. */
 
-require_once(ABSPATH . 'wp-settings.php');
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+    define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once( ABSPATH . 'wp-settings.php' );

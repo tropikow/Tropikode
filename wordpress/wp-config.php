a81e5e379f502691d4a1e48d0eb6c85ef97f2569
TropikoW <jovannypersonal@gmail.com>
2024-08-26 15:46:58 -0500

    Update Dockerfile and docker-compose.yaml to use port 80 for WordPress deployment

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -1,46 +1,21 @@
 <?php
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define( 'DB_NAME', getenv('DB_NAME') );
-
-/** MySQL database username */
-define( 'DB_USER', getenv('DB_USER') );
-
-/** MySQL database password */
-define( 'DB_PASSWORD', getenv('DB_PASSWORD') );
-
-/** MySQL hostname */
-define( 'DB_HOST', getenv('DB_HOST') );
-
-/** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
-
-/** The Database Collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+// ** Configuración de la base de datos ** //
+define('DB_NAME', 'landing_db_wuyr');
+define('DB_USER', 'landing_db_wuyr_user');
+define('DB_PASSWORD', 'zShM0fVFlwHsZbHNA18Jgi8lPPedGkIj');
+define('DB_HOST', 'dpg-cqptqtrqf0us73959860-a.oregon-postgres.render.com'); // Por ejemplo, 'localhost' o la URL de tu base de datos en Render
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
 
 // Configuración para PostgreSQL
 define('DB_TYPE', 'pgsql');
-define('DB_PORT', getenv('DB_PORT')); // Puerto para PostgreSQL
-
-define( 'AUTH_KEY',         getenv('AUTH_KEY') );
-define( 'SECURE_AUTH_KEY',  getenv('SECURE_AUTH_KEY') );
-define( 'LOGGED_IN_KEY',    getenv('LOGGED_IN_KEY') );
-define( 'NONCE_KEY',        getenv('NONCE_KEY') );
-define( 'AUTH_SALT',        getenv('AUTH_SALT') );
-define( 'SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT') );
-define( 'LOGGED_IN_SALT',   getenv('LOGGED_IN_SALT') );
-define( 'NONCE_SALT',       getenv('NONCE_SALT') );
+define('DB_PORT', '5432'); // Puerto predeterminado para PostgreSQL
 
 $table_prefix = 'wp_';
 
-define( 'WP_DEBUG', getenv('WP_DEBUG') );
+define('WP_DEBUG', false);
 
-/* That's all, stop editing! Happy publishing. */
+if ( !defined('ABSPATH') )
+    define('ABSPATH', dirname(__FILE__) . '/');
 
-/** Absolute path to the WordPress directory. */
-if ( ! defined( 'ABSPATH' ) ) {
-    define( 'ABSPATH', dirname( __FILE__ ) . '/' );
-}
-
-/** Sets up WordPress vars and included files. */
-require_once( ABSPATH . 'wp-settings.php' );
+require_once(ABSPATH . 'wp-settings.php');

3cf2e4733b31d016a562f21eeca2514322db813f
TropikoW <jovannypersonal@gmail.com>
2024-09-30 09:38:22 -0500

    Add environment variable for port configuration

--- Dockerfile
+++ Dockerfile
@@ -17,4 +17,4 @@ COPY . .
 EXPOSE 3000
 
 # Comando para ejecutar la aplicación
-CMD ["npm", "start"]
+CMD ["npm", "run", "dev:production"]
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hello, world!');
+const express = require('express');
+const app = express();
+const path = require('path');
+const dotenv = require('dotenv');
+
+const envFile = process.env === 'production' ? '.env' : '.env.dev';
+dotenv.config({path: envFile})
+
+const PORT = process.env.PORT || 3000;
+
+if(process.env.NODE_ENV === 'production'){
+  app.listen(PORT,()=>{
+    console.log(`Server is running on PORT ${PORT} and in ${process.env.NODE_ENV} mode`);
+  })
+} else {
+  app.listen(PORT,()=>{
+    console.log(`Server is running on PORT ${PORT} and in ${process.env.NODE_ENV} mode`);
+  })
+}
